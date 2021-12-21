91935a82f110f0a77ac54e3c78bf81600a2eb36b
Mohammad Salman <mohammadsalman@Mohammads-MacBook-Air.local>
2021-12-21 09:37:46 +0530

    email added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,8 @@ const Card = (props) => {
   return (
     <div className="card-container">
       <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monsters" />
-      <h1>{props.monster.name}</h1>
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   )
 }
